8d424fb6da15f1e8e572cef64a18193fdd0e32fd
rphoon <49342053+rphoon@users.noreply.github.com>
2021-02-12 10:15:59 +1100

    Update setup.ps1

--- Uploads/WVDScripts/001-AzFiles/setup.ps1
+++ Uploads/WVDScripts/001-AzFiles/setup.ps1
@@ -19,20 +19,16 @@ Azure admin password
 #>
 
 param(    
-
-
+    [Parameter(Mandatory = $true)]
+    [string] $I,
     [Parameter(Mandatory = $true)]
     [string] $RG,
-
     [Parameter(Mandatory = $true)]
     [string] $S,
-
     [Parameter(Mandatory = $true)]
     [string] $U,
-
     [Parameter(Mandatory = $true)]
     [string] $P
-
 )
 
 # Set execution policy    
@@ -45,27 +41,26 @@ Set-Location $PSScriptroot
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name PowershellGet -MinimumVersion 2.2.4.1 -Force
 
-Install-Module -Name Az -Force -Verbose
-
+Install-Module -Name Az.Accounts -Force -Verbose
 Import-Module -Name AzFilesHybrid -Force -Verbose
-Import-Module -Name activedirectory -Force -Verbose
+
+#Import-Module -Name activedirectory -Force -Verbose
 
 # Find existing OU or create new one. Get path for OU from domain by splitting the domain name, to format DC=fabrikam,DC=com
-$domain = $U.split('@')[1]
-$DC = $domain.split('.')
-foreach($name in $DC) {
-    $path = $path + ',DC=' + $name
-}
-$path = $path.substring(1)
-$ou = Get-ADOrganizationalUnit -Filter 'Name -like "Profiles Storage"'
-if ($ou -eq $null) {
-    New-ADOrganizationalUnit -name 'Profiles Storage' -path $path
-}
+#$domain = $U.split('@')[1]
+#$DC = $domain.split('.')
+#foreach($name in $DC) {
+#    $path = $path + ',DC=' + $name
+#}
+#$path = $path.substring(1)
+#$ou = Get-ADOrganizationalUnit -Filter 'Name -like "Profiles Storage"'
+#if ($ou -eq $null) {
+#    New-ADOrganizationalUnit -name 'Profiles Storage' -path $path
+#}
 
 # Connect to Azure
 $Credential = New-Object System.Management.Automation.PsCredential($U, (ConvertTo-SecureString $P -AsPlainText -Force))
 Connect-AzAccount -Credential $Credential
-$context = Get-AzContext
-Select-AzSubscription -SubscriptionId $context.Subscription.Id
+Set-AzContext -SubscriptionId $I
 
-Join-AzStorageAccountForAuth -ResourceGroupName $RG -StorageAccountName $S -DomainAccountType 'ComputerAccount' -OrganizationalUnitName 'Profiles Storage' -OverwriteExistingADObject
+Join-AzStorageAccountForAuth -ResourceGroupName $RG -StorageAccountName $S -DomainAccountType 'ComputerAccount' -OverwriteExistingADObject
