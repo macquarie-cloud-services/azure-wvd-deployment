0e5a8f298bf0df25ff4d0094af2818ceb9ea7aee
rphoon <49342053+rphoon@users.noreply.github.com>
2021-02-17 22:22:18 +1100

    Update sysprep.ps1

--- ARMRunbookScripts/sysprep.ps1
+++ ARMRunbookScripts/sysprep.ps1
@@ -3,10 +3,12 @@ param([switch]$runSysprep=$false)
 write-output "Sysprep Script Run, parameter 'runSysprep': $runSysprep"
 
 if ($runSysprep) {
-  write-output "starting Sysprep"
+  write-output "`nDeleting C:\Windows\Panther directory..."
+  Remove-Item C:\windows\Panther\test -Recurse -Force
+  
+  write-output "`nStarting Sysprep..."
   Start-Process -FilePath C:\Windows\System32\Sysprep\Sysprep.exe -ArgumentList '/generalize /oobe /shutdown /quiet'
-  write-output "started Sysprep"
 }
 else {
-  write-output "skipping Sysprep"
+  write-output "`nSkipping Sysprep..."
 }
