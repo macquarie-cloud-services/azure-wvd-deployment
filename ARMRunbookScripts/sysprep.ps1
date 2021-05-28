53f79d5bdc9fead2536cc848781b96b0b01006ce
rphoon <49342053+rphoon@users.noreply.github.com>
2021-05-29 00:07:33 +1000

    Update sysprep.ps1

--- ARMRunbookScripts/sysprep.ps1
+++ ARMRunbookScripts/sysprep.ps1
@@ -1,11 +1,8 @@
 # Run sysprep in vm mode
 
-write-output "Sysprep Script Run, parameter 'runSysprep': $runSysprep"
-
 If (Test-Path -Path "C:\windows\Panther") {
   Write-output "`nDeleting C:\Windows\Panther directory..."
   Remove-Item C:\windows\Panther -Recurse -Force
 }
 Write-output "`nStarting Sysprep..."
 Start-Process -FilePath C:\Windows\System32\Sysprep\Sysprep.exe -ArgumentList '/generalize /oobe /shutdown /mode:vm' -Wait
-
